47f96ed74ac486bc1dc343fc68fbdfb60dd4358a
Miguel Huayhua <miguelhuayhuac@gmail.com>
2022-02-26 19:17:57 -0400

    listo

--- index.js
+++ index.js
@@ -8,25 +8,7 @@ const cors = require('cors');
 const nodei3 = require('node-id3').Promise
 const fs = require('fs');
 
-app.use(function (req, res, next) {
-    req.headers.authorization
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://ytdownmike.netlify.app/');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/descargas', route)
@@ -39,7 +21,7 @@ app.get('/', (req, res) => {
 })
 
 
-app.post('/', cors({ origin: 'https://ytdownmike.netlify.app/' }), (req, res, next) => {
+app.post('/', cors(), (req, res, next) => {
     let link = req.body.link
     const info = ytdl.getInfo(link).then(info => {
         let musica = {
